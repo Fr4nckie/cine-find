79758f3d4e1666b1eb32dc401d157242ceea3976
Franckie <franckie.dev@gmail.com>
2025-11-03 15:37:51 +0300

    refactor(components): move Header into its own folder and update imports

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom"
-import Header from "../components/Header.tsx"
+import Header from "../components/Header/Header.tsx"
 import Footer from "../components/Footer.tsx"
 
 const Root = () => {
